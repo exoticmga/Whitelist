f5fd7a2da1aec99c9c69023b914c4b4a4528cc11
exoticmga <agwumbaagha@gmail.com>
2022-04-11 14:18:57 +0100

    pal

---
+++ my-app/constants/index.js
@@ -0,0 +1,69 @@
+/*jshint esversion: 9*/
+export const abi =  [
+      {
+        "inputs": [
+          {
+            "internalType": "uint8",
+            "name": "_maxWhitelistedAddresses",
+            "type": "uint8"
+          }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+      },
+      {
+        "inputs": [],
+        "name": "addAddressToWhitelist",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "inputs": [],
+        "name": "maxWhitelistedAddresses",
+        "outputs": [
+          {
+            "internalType": "uint8",
+            "name": "",
+            "type": "uint8"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [],
+        "name": "numAddressesWhitelisted",
+        "outputs": [
+          {
+            "internalType": "uint8",
+            "name": "",
+            "type": "uint8"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "address",
+            "name": "",
+            "type": "address"
+          }
+        ],
+        "name": "whitelistedAddresses",
+        "outputs": [
+          {
+            "internalType": "bool",
+            "name": "",
+            "type": "bool"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      }
+    ];
+    
+
+export const WHITELIST_CONTRACT_ADDRESS = "0x72B8aCAdd4C1C97680d8BA75A37c4254732c603b";
